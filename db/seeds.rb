f1c42e4d4eb93ea0c4a0e2207226de4fc7d1e298
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-08-24 13:37:25 -0700

    Completed Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: 'Dwayne', last_name: 'Johnson', grade: 5)
+Student.create!(first_name: 'Pablo', last_name: 'Picasso', grade: 12)
+Student.create!(first_name: 'Marilyn', last_name: 'Monroe', grade: 3)
